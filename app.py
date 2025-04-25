a404678f5fd6498bc2c4e89ad79945cff6d29926
Atul-728 <atulkumar805185@gmail.com>
2025-04-25 13:45:36 +0530

    Updated

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def predictor():
     return render_template('predictor.html', prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
